59bbf30625141f8de56341b941d3e84fe92dfcfa
shely25 <shely.ece.25@gmail.com>
2022-05-27 16:44:03 +0600

    tools object created and mongo connecteted

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 const express = require('express')
+const cors = require('cors');
 const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+require('dotenv').config()
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dyrm9.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const toolsColletions = client.db("toolsCollection").collection("Tools");
+        app.get('/tools', async (req, res) => {
+            const query = {}
+            const cursor = toolsColletions.find(query);
+            const result = await cursor.toArray()
+            res.send(result)
+        }
+        )
+    }
+
+    finally {
+    }
+}
+run().catch(console.dir);
+
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
